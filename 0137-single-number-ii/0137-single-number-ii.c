804947955cdc0e57642699c9a9849918593e7160
Orshitha16 <orshithavaishali04@gmail.com>
2025-07-28 14:05:04 +0530

    Time: 139 ms (19.07%), Space: 9.1 MB (38.64%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.c
@@ -0,0 +1,27 @@
+int singleNumber(int* nums, int numsSize) {
+    int i,j;
+    int f,k,c;
+    for(i=0;i<numsSize;i++)
+    { f=0;
+    for(j=0;j<i;j++)
+    if(nums[i]==nums[j])
+    {
+        f=1;
+        break;
+    }
+    
+    if(f==0)
+    { c=0;
+    for(j=i;j<numsSize;j++)
+    if(nums[i]==nums[j])
+    c++;
+    }
+    if(c==1)
+    {
+    k= nums[i];
+    break;
+    }
+    
+}
+return k;
+}
